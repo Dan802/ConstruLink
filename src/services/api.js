2616b7d7eab4a2349bea42c0b636f6a703d59a43
Dan802 <jfelipeg05@gmail.com>
2025-11-06 12:48:08 -0500

    hardcode because yes

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
 console.log("LA API ESTA EN ", API_URL)
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://constru-link-back-end.vercel.app",
   headers: { "Content-Type": "application/json" },
 });
 
